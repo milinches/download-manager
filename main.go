c8a82413866ad6b70877729009f3e63c0c84269c
milinches <ibrogenius5165@gmail.com>
2022-01-24 18:04:26 +0100

    made changes

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -24,8 +26,60 @@ func (d *Download) Do() error {
 	if err != nil {
 		return err
 	}
-
 	fmt.Printf("Got: %v\n", resp.StatusCode)
+
+	if resp.StatusCode > 299 {
+		return errors.New(fmt.Sprintf("Can't process, response is %v\n", resp.StatusCode))
+	}
+
+	size, err := strconv.Atoi(resp.Header.Get("Content-Length"))
+	if err != nil {
+		fmt.Printf("Size is %v bytes\n", size)
+	}
+
+	sections := make([][2]int, d.totalSection)
+	eachSize := size / d.totalSection
+	fmt.Printf("Each Size is %v bytes\n", eachSize)
+	
+	for i := range sections {
+		if i == 0 {
+			sections[i][0] = 0
+		} else {
+			sections[i][0] = sections[i-1][1] + 1
+		}
+
+		if i < d.totalSection-1 {
+			sections[i][1] = sections[i][0] + eachSize
+		} else {
+			sections[i][1] = size - 1
+		}
+	}
+	fmt.Println(sections)
+
+	for i, s := range sections {
+		err = d.DownloadSession(i, s)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (d *Download) DownloadSession(i int, c[2]int) error {
+	r, err := d.GetNewRequest("GET")
+	if err != nil {
+		return err
+	}
+	r.Header.Set("Range", fmt.Sprintf("bytes=%v-%v", c[0], c[1]))
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode > 299 {
+		return errors.New(fmt.Sprintf("Can't process, response is %v", resp.StatusCode))
+	}
 
 	return nil
 }
@@ -43,8 +97,8 @@ func (d *Download) GetNewRequest(method string) (*http.Request, error) {
 func main() {
 	startTime := time.Now()
 	d := Download{
-		url: "file:///C:/Users/HP/Documents/Languages/Rust/rust.pdf",
-		targetPath: "rust.pdf",
+		url: "https://github.com/dappuniversity/eth-todo-list/archive/refs/heads/master.zip",
+		targetPath: "master.zip",
 		totalSection: 10,
 	}
 
